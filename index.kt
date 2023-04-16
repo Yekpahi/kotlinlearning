4690d70ae876e1243a8471c22c004eef2113fdde
Yekpahi Prince <princeyekpahi@gmail.com>
2023-04-16 16:25:56 +0200

    Kotlin User-defined Functions

--- index.kt
+++ index.kt
@@ -1,7 +1,18 @@
-fun main(args: Array<String>) {
-    hello()
+fun main() {
+    println("the of number 10 is : ${getSquare(10)}")
+    gettriple(5)
 }
 
-fun hello() {
-    println("Hello")
+// Return fonction
+fun getSquare(number : Int): Int {
+    val square = number * number
+    return square
+
+}
+
+fun gettriple(number : Int) {
+    val triple = number * number * number
+    
+    println("the triple of $number is : ${triple}")
+
 }
